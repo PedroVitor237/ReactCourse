c4b2f179ebad12075ca0cc2ef118f7ac35428fa5
Pedro Vitor <pedrovitor.deva@gmail.com>
2026-05-21 17:04:43 -0300

    feat: update App component and add AppDefault component with initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const defaultMessage = "This is my first React app!";
+
+    // State: is a way to store and manage data in a React component. It allows us to create dynamic and interactive user interfaces.
+    const [message, setMessage] = useState("Click the button!");
+
+    return (
+
+        <div>
+            <h1>Hello World!</h1>
+            <p>{defaultMessage}</p>
+
+            <button onClick={() => setMessage("You clicked the button!")}>
+                {message}
+            </button>
+        </div>
+
+    );
 }
 
-export default App
+export default App;
